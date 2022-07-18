4a5f0c71a445196e8131eaa39531400bbad3b3d0
vladkrasovsky <v.o.krasovsky@gmail.com>
2022-07-18 22:39:49 +0000

    Deploying to gh-pages from @ vladkrasovsky/icecream@4a7e98db66ef9108dd79d541b04b635466c03f67 🚀

---
+++ index.c6b73b63.js
@@ -0,0 +1,2 @@
+!function(){var e;(e=document.querySelector("[data-hidden-text-toggler]")).addEventListener("click",(function(){e.classList.toggle("is-open")}))}();
+//# sourceMappingURL=index.c6b73b63.js.map
---
+++ index.cb93205e.js
@@ -0,0 +1,2 @@
+(()=>{const e=document.querySelector("[data-hidden-text-toggler]");e.addEventListener("click",(()=>{e.classList.toggle("is-open")}))})();
+//# sourceMappingURL=index.cb93205e.js.map
