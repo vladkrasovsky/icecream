2fe8cf0835a2e8c257d2c85446379cf6ab8b2a91
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2022-07-18 23:58:27 +0300

    Replaces counter script with library

--- src/js/counter.js
+++ src/js/counter.js
@@ -1,20 +1,22 @@
-const counters = document.querySelectorAll ('.counter');
-const speed = 1000;
+$(document).ready(function () {
+  var show = true;
+  var countbox = '.advantages__list';
+  $(window).on('scroll load resize', function () {
+    if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+    var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+    var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+    var w_height = $(window).height(); // Высота окна браузера
+    var d_height = $(document).height(); // Высота всего документа
+    var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+    if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+      $('.js-count').css('opacity', '1');
+      $('.js-kg').css('opacity', '1');
+      $('.js-count').spincrement({
+        thousandSeparator: '',
+        duration: 1200,
+      });
 
-counters.forEach(counter =>{
-    const updateCount = () => {
-        const target = counter.getAttribute('data-target');
-        const count = +counter.innerText;
-
-        const inc = target / speed;
-
-        if(count < target) {
-            counter.innerText = Math.ceil(count +inc);
-            setTimeout(updateCount, 1);
-        } else {
-            count.innerText = target;
-        }
+      show = false;
     }
-
-    updateCount();
+  });
 });
